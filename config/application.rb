c742b9f52d511492ebe3932fccdafd5ff85879fa
tammkage0915 <280695020+tammkage0915@users.noreply.github.com>
2026-08-03 04:23:45 +0000

    deploy setting

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Tabinoato
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_support.use_solid_cache = false
   end
 end
